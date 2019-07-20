a3ac136f95fd22e26a184ddb9f03df9adae6a467
Bourne-ID <cam40902@gmail.com>
2019-07-20 23:11:29 +0100

    Fix tagging

--- travis/build.sh
+++ travis/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e # Fail Fast
 #USAGE: ./build.sh [Dockerfile location]
 
 file=$1
@@ -12,14 +13,7 @@ tag="${tag//[\/\.]/-}"  #ubuntu-14-04
 echo "Tag: ${tag}"
 # Docker build
 cd docker
-
-pwd=$(pwd)
-echo "PWD: ${pwd}"
 docker build -t "bourneid/linuxgsm-test:${tag}" -f ${file} .
-
-if [[ $? -ne 0 ]]; then
-    exit 1
-fi
-
 docker login -u "${QUAY_BOT_USERNAME}" -p "{$QUAY_BOT_PASSWORD}" quay.io
+docker tag bourneid/linuxgsm-test:${tag} quay.io/bourneid/linuxgsm-test:${tag}
 docker push quay.io/bourneid/linuxgsm-test:${tag}
