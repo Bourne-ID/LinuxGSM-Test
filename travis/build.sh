af572fcf76a43185525a04973288a839eb5116f5
Bourne-ID <cam40902@gmail.com>
2019-07-20 23:18:16 +0100

    minor fix, but an important one!

--- travis/build.sh
+++ travis/build.sh
@@ -14,6 +14,6 @@ echo "Tag: ${tag}"
 # Docker build
 cd docker
 docker build -t "bourneid/linuxgsm-test:${tag}" -f ${file} .
-docker login -u "${QUAY_BOT_USERNAME}" -p "{$QUAY_BOT_PASSWORD}" quay.io
+docker login -u "${QUAY_BOT_USERNAME}" -p "${QUAY_BOT_PASSWORD}" quay.io
 docker tag bourneid/linuxgsm-test:${tag} quay.io/bourneid/linuxgsm-test:${tag}
 docker push quay.io/bourneid/linuxgsm-test:${tag}
